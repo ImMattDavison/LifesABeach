f4a32fe233efa7043560f74384234ece8c6c92d5
Matt <matt@spectrebot.net>
2023-06-16 21:27:40 +0100

    add basic home content

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,10 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
     return (
         <>
-        
+            <main className={styles.main}>
+                <h1 className={styles.title}>Life's a Beach</h1>
+                <p className={styles.description}>Don't get washed away in the sea of terminology.</p>
+            </main>
         </>
     )
 }

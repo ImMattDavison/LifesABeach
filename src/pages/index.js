b97f9e9c925839acbb7d2c87f04f0b449ee646d8
Matt <matt@spectrebot.net>
2023-06-17 19:09:18 +0100

    update index buttons

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
+import { Col, Row, Container } from 'react-bootstrap'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -11,6 +12,11 @@ export default function Home() {
             <main className={styles.main}>
                 <h1 className={styles.title}>Life's a Beach</h1>
                 <p className={styles.description}>Don't get washed away in the sea of terminology.</p>
+                <p>The API and Info site to save you!</p>
+                <Row>
+                    <a className={styles.button} href='/directories/pronouns'>Pronouns</a>
+                    <a className={styles.button} href='/directories/sexualities'>Sexualities</a>    
+                </Row>
             </main>
         </>
     )

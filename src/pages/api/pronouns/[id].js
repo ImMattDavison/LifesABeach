a26c0ae7651f7bbde7ff6c91a776ef78abbed054
Matt <matt@spectrebot.net>
2023-06-17 09:09:02 +0100

    pull json data into api

--- src/pages/api/pronouns/[id].js
+++ src/pages/api/pronouns/[id].js
@@ -1,4 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import data from '/public/json/pronouns.json'
 
 export default function handler(req, res) {
     console.log(req.query)

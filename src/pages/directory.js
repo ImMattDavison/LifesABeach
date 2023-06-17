6b61d9775ee8e7fd69dbf433322dc7a095f6f20a
Adam Drummond <adam.drummond9@gmail.com>
2023-06-17 13:11:23 +0100

    added pronoun directory

--- src/pages/directory.js
+++ src/pages/directory.js
@@ -1,6 +1,7 @@
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 import { Col, Row } from 'react-bootstrap'
+import data from '/public/json/pronouns.json'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -11,14 +12,21 @@ export default function Home() {
                 <h1 className={styles.title}>Pronoun Directory</h1>
                 <p className={styles.description}>Don't get washed away in the sea of terminology.</p>
 
-                <Row>
-                    <Col xs={{span: 10, offset:1}} sm={{span: 5, offset: 1}} md={{span: 4, offset: 2}} lg={{span: 3, offset: 2}}>colcolcolcol</Col>
-                    <Col xs={{span: 10, offset: 1}} sm={{span: 5}} md={{span: 4}} lg={{span: 3}}>colcolcolcol</Col>
-                    <Col xs={{span: 10, offset: 1}} sm={{span: 5, offset: 1}} md={{span: 4, offset: 2}} lg={{span: 3}}>colcolcolcol</Col>
-                    <Col xs={{span: 10, offset:1}} sm={{span: 5}} md={{span: 4}} lg={{span: 3, offset: 1}}>colcolcolcol</Col>
-                    <Col xs={{span: 10, offset: 1}} sm={{span: 5, offset: 1}} md={{span: 4, offset: 2}} lg={{span: 3}}>colcolcolcol</Col>
-                    <Col xs={{span: 10, offset: 1}} sm={{span: 5}} md={{span: 4}} lg={{span: 3}}>colcolcolcol</Col>
+                <Row className={styles.description}>
+                    {
+                        data.pronouns.map(pronoun => {
+                            console.log("pronoun");
+                            return <Col xs={12} sm={6} md={4} lg={3}>
+                                <a href={"/api/pronouns/" + pronoun.short.join("-")} className={styles.description}>
+                                    {
+                                        pronoun.short.join("-")
+                                    }
+                                </a>
+                            </Col>
+                        })
+                    }
                 </Row>
+                
 
             </main>
         </>

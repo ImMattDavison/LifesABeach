53cb5ea5d7e20c377489ce3ed7061125b6fd5934
Matt <matt@spectrebot.net>
2023-06-18 14:38:51 +0100

    add /blahaj redirect

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
+
+    async redirects() {
+        return [
+            {
+                source: '/blahaj',
+                destination: 'https://discord.gg/763bUceUHb',
+                permanent: true,
+            },
+        ]
+    },
 }
 
 module.exports = nextConfig
